6391b1fdffd879824a9a03a09705f3236036c442
Andrew McGill <arm5077@gmail.com>
2014-11-21 14:55:23 -0500

    select and display single image + caption

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,15 +1,29 @@
 
 $(document).ready(function(){
+	images = [];
+	index = 0;
+	
 	// Pull local JSON file of images
 	$.getJSON("data/images.json", function(json){
 		if(json.status.code == "200") {
 			$.each(json.results, function(i, image){
-				$("<img>").appendTo(".slideshow .image")
-					.attr("src", "http://www.post-gazette.com" + image.url);
+				insertedImage = $("<img>").appendTo(".slideshow .image")
+					.attr("src", "http://www.post-gazette.com" + image.url)
+					.attr("alt", image.caption);
+				images.push(insertedImage);
 			});
+			
+			activateImage(images[4]);
+			
 		} else {
 			console.log("Error: " + json.status.description);
 		}
 	});
 
 });
+
+function activateImage(image, caption){
+	$(".slideshow img").removeClass("visible");
+	$(image).addClass("visible");
+	$(".caption").text(image.attr("alt"));
+}
